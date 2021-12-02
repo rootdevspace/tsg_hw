6c50e34b574957859620c02e7b9661a7c47f4ffc
rootdevspace <voloshyn0988@gmail.com>
2021-12-02 19:13:19 +0200

    new commit

--- unit1/index.php
+++ unit1/index.php
@@ -1,13 +1,17 @@
-<!doctype html>
 <html>
-    <body>
-        <?php
-        
-        if(date('H:i:s',time())>=date('H:i:s',mktime(3,0,0))&&date('H:i:s',time())<date('H:i:s',mktime(11,0,0))){
-            echo 'Good morning world!';
-        }else{
-            echo 'Hello world!';
-        }
-        ?>
-    </body>
+<head>
+    <title>Задача 3</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+
+<?php
+if (!date('H:i:s',time())>=date('H:i:s',mktime(3,0,0))&&!date('H:i:s',time())<date('H:i:s',mktime(11,0,0))) {
+    echo "Привіт, світ!";
+} else {
+    echo "Добрий ранок, світ!";
+}
+?>
+
+</body>
 </html>
